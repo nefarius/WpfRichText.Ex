61661090a59b0c43dad9882c4095fa5ac36b2006
Benjamin Höglinger <nefarius@dhmx.at>
2012-03-07 18:44:33 +0100

    Initial commit Already applied patches from the original comments

---
+++ WpfRichText.Ex/Controls/RichTextEditor.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfRichText.Ex.Controls
+{
+    /// <summary>
+    /// Interaction logic for BindableRichTextbox.xaml
+    /// </summary>
+    public partial class RichTextEditor : UserControl
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(RichTextEditor),
+            new PropertyMetadata(string.Empty));
+        public static readonly DependencyProperty ToolbarVisibilityProperty =
+            DependencyProperty.Register("ToolbarVisibility", typeof(Visibility), typeof(RichTextEditor),
+            new PropertyMetadata(Visibility.Visible));
+
+        public RichTextEditor()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return GetValue(TextProperty) as string; }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+
+        public Visibility ToolbarVisibility
+        {
+            get { return (Visibility)GetValue(ToolbarVisibilityProperty); }
+            set
+            {
+                SetValue(ToolbarVisibilityProperty, value);
+            }
+        }
+    }
+}
