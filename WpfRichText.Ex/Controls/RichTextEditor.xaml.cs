2e0eafdd5f4531085c8be0033f42a1c97918ee13
nefarius <nefarius@dhmx.at>
2012-03-09 10:02:29 +0100

    Added README.TXT Fixed incorrect data type of EditorHeight property

--- WpfRichText.Ex/Controls/RichTextEditor.xaml.cs
+++ WpfRichText.Ex/Controls/RichTextEditor.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfRichText.Ex.Controls
 {
@@ -52,9 +41,9 @@ namespace WpfRichText.Ex.Controls
             }
         }
 
-        public double EditorHeight
+        public string EditorHeight
         {
-            get { return (double)GetValue(ToolbarVisibilityProperty); }
+            get { return (string)GetValue(ToolbarVisibilityProperty); }
             set
             {
                 SetValue(ToolbarVisibilityProperty, value);
